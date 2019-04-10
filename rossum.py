060a9302bd29ebbd196a3e1f297fb01ceecfff01
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2019-04-10 11:11:01 +0200

    Update copyright.

--- rossum.py
+++ rossum.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2016, G.A. vd. Hoorn
+# Copyright (c) 2016-2019 G.A. vd. Hoorn
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
