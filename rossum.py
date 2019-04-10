cdad9c9972f6f962847ddbb46d0c75c80b407d4a
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2019-04-10 10:47:22 +0200

    Harmonise path separators in search paths.

--- rossum.py
+++ rossum.py
@@ -53,18 +53,18 @@ BUILD_FILE_NAME='build.ninja'
 BUILD_FILE_TEMPLATE_NAME='build.ninja.em'
 
 FANUC_SEARCH_PATH = [
-    'C:\\Program Files\\Fanuc',
-    'C:\\Program Files (x86)\\Fanuc',
-    'D:\\Program Files\\Fanuc',
-    'D:\\Program Files (x86)\\Fanuc',
+    'C:/Program Files/Fanuc',
+    'C:/Program Files (x86)/Fanuc',
+    'D:/Program Files/Fanuc',
+    'D:/Program Files (x86)/Fanuc',
 ]
 
 KTRANS_BIN_NAME='ktrans.exe'
 KTRANS_SEARCH_PATH = [
-    'C:\\Program Files\\Fanuc\\WinOLPC\\bin',
-    'C:\\Program Files (x86)\\Fanuc\\WinOLPC/bin',
-    'D:\\Program Files\\Fanuc\\WinOLPC\\bin',
-    'D:\\Program Files (x86)\\Fanuc\\WinOLPC/bin',
+    'C:/Program Files/Fanuc/WinOLPC/bin',
+    'C:/Program Files (x86)/Fanuc/WinOLPC/bin',
+    'D:/Program Files/Fanuc/WinOLPC/bin',
+    'D:/Program Files (x86)/Fanuc/WinOLPC/bin',
 ]
 
 KTRANSW_BIN_NAME='ktransw.cmd'
